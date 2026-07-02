441bdd4d7e5ec0aec0c5addfceacb3775774712d
lu-lui <lpneutzling@gmail.com>
2026-07-01 23:39:20 -0300

    remocao de valores

--- estudos/arvores/remocao1.c
+++ estudos/arvores/remocao1.c
@@ -1,7 +1,6 @@
 //árvore binária
-//processo de inserção curto
-//descobrindo o tamanho
-//busca de elemento
+//remoção de um elemento
+//remoção 1: remover o nó folha (não tem nenhum filho)
 
 #include <stdio.h>
 #include <stdlib.h>
@@ -12,17 +11,18 @@ typedef struct no{
     struct no *direita;
 }No;
 
-No* inserir(No *raiz, int valor); //agora retorna um nó
+No* inserir(No *raiz, int valor); 
 void imprimir(No *arvore);
 int tamanho(No *raiz);
 int buscar(No *raiz, int chave);
+No* remover(No *raiz,int chave);
 
 int main(){
     int op, valor;
     No *raiz = NULL;
     
     do{
-        printf("O - sair \n1 - inserir \n2 - imprimir \n3 - Buscar\n");
+        printf("O - sair \n1 - inserir \n2 - imprimir \n3 - Buscar \n4- Remover\n");
         scanf("%d", &op);
 
         switch (op){
@@ -47,6 +47,12 @@ int main(){
             scanf("%d", &valor);
             printf("\nResultado da busca: %d\n", buscar(raiz, valor));
             break;
+
+        case 4:
+            printf("Digite um valor para remoção: ");
+            scanf("%d", &valor);
+            raiz = remover(raiz, valor);
+            break;
         
         default:
             printf("Opcao invalida\n");
@@ -58,16 +64,15 @@ int main(){
 }
 
 No* inserir(No *raiz, int valor){
-    //arvore vazia (raiz nula)
-    if(raiz == NULL){ //árvore vazia
+    if(raiz == NULL){ 
         No *novo = (No *)malloc(sizeof(No));
-        novo->valor = valor;      //novo nó recebe o valor
-        novo->esquerda = NULL;    //ponteiros não apontam para nada
+        novo->valor = valor;      
+        novo->esquerda = NULL;    
         novo->direita = NULL;
         return novo;
     } else { //raiz não é nula
         if (valor < raiz->valor)
-            raiz->esquerda = inserir(raiz->esquerda, valor); //chamada recursiva
+            raiz->esquerda = inserir(raiz->esquerda, valor); 
         if (valor > raiz->valor)
             raiz->direita = inserir(raiz->direita, valor);
         return raiz;
@@ -76,35 +81,52 @@ No* inserir(No *raiz, int valor){
 
 void imprimir(No *raiz){
     if( raiz != NULL){
-        imprimir(raiz->esquerda); //imprime esquerda
-        printf("%d ", raiz->valor); //imprime raiz
-        imprimir(raiz->direita); //imprime direita
+        imprimir(raiz->esquerda); 
+        printf("%d ", raiz->valor); 
+        imprimir(raiz->direita); 
     }
 }
 
-//chamada recursiva para descobrir o tamanho
 int tamanho(No *raiz){
     if (raiz == NULL)
         return 0;
     else
         return 1 + tamanho(raiz->esquerda) + tamanho(raiz->direita);
-    // 1 == nó raiz + soma dos nós da esquerda/direita
 }
 
-//para só saber se o conteúdo está ou não, mudar os retornos para 0/1 (true/false)
 int buscar(No *raiz, int chave){
     if (raiz == NULL){
-        return -1; //retornar número que nunca fará parte da árvore binária (ex: árvore de valores positivos)
-        //return 0;
+        return -1; 
     } else {
         if (raiz->valor == chave)
-            return raiz->valor; // ou return chave;
-            //return 1;
+            return raiz->valor; 
         else {
-            if (chave < raiz->valor) //aqui se elimina metade dads possibilidades, por isso a complexidade é O(Log n)
-                return buscar(raiz->esquerda, chave);  //chamada recursiva
+            if (chave < raiz->valor) 
+                return buscar(raiz->esquerda, chave);  
             else
-                return buscar(raiz->direita, chave);  //chamada recursiva
+                return buscar(raiz->direita, chave); 
         }
     }     
+}
+
+No* remover(No *raiz,int chave){
+    if (raiz == NULL){ //árvore vazia
+        printf("Valor não encontrado\n");
+        return NULL; //esse null
+    } else {
+        if(raiz->valor == chave){ //encontrou o valor na raiz (remoção para nós folha)
+            if (raiz->esquerda == NULL && raiz->direita == NULL){
+                free(raiz);
+                return NULL;
+            }
+            
+        } else {
+            if (chave < raiz->valor) //não encontrou na raiz, se é menor, vai para a esquerda
+                raiz->esquerda = remover(raiz->esquerda, chave); //raiz->esquerda recebe o retorno NULL de remover
+            else
+                raiz->direita = remover(raiz->direita, chave); 
+                //precisa retornar o endereço para não perder os ponteiros que ligam a arvore
+            return raiz;
+        }
+    }
 }
