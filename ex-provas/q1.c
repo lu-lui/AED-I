a94fffd95e65a26a64967e0706c79d1d3c80388b
lu-lui <lpneutzling@gmail.com>
2026-07-15 01:44:07 -0300

    resolucao questao 1

--- ex-provas/q1.c
+++ ex-provas/q1.c
@@ -18,27 +18,27 @@ void main() {
     v[2] = 3;
     v[3] = 2;
     v[4] = 1;
-    set(&i, 0);
-    set(&h, 0);
-    p = &h;
-    while (i < 3) {
-        printf("%d\n", h);
-        soma(p, i);
-        i++;
+    set(&i, 0); // i = 0
+    set(&h, 0); // h = 0
+    p = &h; // p -> h = 3
+    while (i < 3) { // i == 0 ==> i == 1 ==> i == 2
+        printf("%d\n", h); // 0 ==> 0 ==> 1
+        soma(p, i); // p -> h = (0 + i == 0) = 0 ==> p -> h = (0 + i == 1) = 1 ==> p -> h = (1 + i == 2) = 3
+        i++; //i = 1 ==> i = 2 ==> i == 3
     }
-    n = r + i;
-    printf("n = %d\n", n);
-    printf("H = %d\n", *p);
+    n = r + i; // n = r=0 + i=3 == 3
+    printf("n = %d\n", n); // n = 3
+    printf("H = %d\n", *p); // H = 5
 
-    set(&v[0], 20);
-    p = &v[0];
-    *p = 90;
-    set(&r, n + 2);
-    set(&n, n * n);
-    printf("v[0] = %d\n", *p);
-    printf("r = %d\n", r);
-    printf("n = %d\n", n);
-    printf("P = %d\n", *(p + 1));
-    p++;
-    printf("P = %d\n", *(p + 1));
+    set(&v[0], 20); //v[0] = 20
+    p = &v[0]; // p -> v[0] = 20
+    *p = 90; // p -> v[0] = 90
+    set(&r, n + 2); // r=0 = n=3 + 2 == 5
+    set(&n, n * n); //n=3 = 3 * 3 == 9 
+    printf("v[0] = %d\n", *p); //v[0] = 90
+    printf("r = %d\n", r); // r = 5
+    printf("n = %d\n", n); // n = 9
+    printf("P = %d\n", *(p + 1)); // P -> v[0] ==> P + 1 -> v[0+1] ==> P + 1 -> v[1] == 4
+    p++; // p -> V[1]
+    printf("P = %d\n", *(p + 1)); // P -> v[1] ==> P + 1 -> v[1+1] ==> P + 1 -> v[2] == 3
 }
